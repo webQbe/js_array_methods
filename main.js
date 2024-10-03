a349187f1c78f46b2ba7b56c4572968f2395b205
webQbe <webcubesolutionslk@gmail.com>
2024-10-03 18:30:52 +0530

    Using filter method with arrow function

--- main.js
+++ main.js
@@ -16,6 +16,13 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 // filter age 21 and over 
 
+
+// filter() with arrow function
+const drinkAges = ages.filter(age => age >= 21)
+
+console.log(drinkAges);
+
+
 // using filter()
 const can_Drink = ages.filter(function(age){
 
