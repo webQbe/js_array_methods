4c3bf8c139c8240ecd276dd69ee9e7a4d71b2f31
webQbe <webcubesolutionslk@gmail.com>
2024-10-03 19:11:21 +0530

    map method used to output company name, start and end years

--- main.js
+++ main.js
@@ -16,13 +16,14 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // map()
 // create array of company names using map()
 
-const companyNames = companies.map(function(company){
+const companyMap = companies.map(function(company){
 
-    return company.name;
+    // output company name, start and end years
+    return  `${company.name} [${company.start} - ${company.end}]`;
 
 });
 
-console.log(companyNames);
+console.log(companyMap);
 
 
 
