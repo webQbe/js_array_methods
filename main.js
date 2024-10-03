a838a03e476c8c1ab1a6136ebd85c36a3fa5da6e
webQbe <webcubesolutionslk@gmail.com>
2024-10-03 19:31:34 +0530

    map method used to create array of square roots of ages multiplied by 2

--- main.js
+++ main.js
@@ -16,6 +16,12 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // map()
 // create arrays using map()
 
+// create array of square roots of ages multiplied by 2
+const ageSqrtDoubled = ages
+.map(age => Math.sqrt(age))
+.map(age => age * 2)
+console.log(ageSqrtDoubled);
+
 // create array of doubled ages
 const ageDoubled = ages.map(age => age * 2);
 console.log(ageDoubled);
