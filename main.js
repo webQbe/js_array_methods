1fa3ffeee2a22a071a7f828efabca0a7b4c38ecc
webQbe <webcubesolutionslk@gmail.com>
2024-10-03 20:04:38 +0530

    used reduce method to add all ages together

--- main.js
+++ main.js
@@ -15,6 +15,22 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 
 // reduce()
+// takes function and start value 
+
+// arrow function
+const totalAges = ages.reduce((total, age) => (total + age), 0);
+console.log(totalAges);
+
+// es5
+const ageTotal = ages.reduce(function(total, age){
+
+    return total + age
+
+}, 0);
+console.log(ageTotal);
+
+
+
 
 // add all ages togther 
 
