d9b0c4471d96b263d39362a7061bbf9c22cb5727
webQbe <webcubesolutionslk@gmail.com>
2024-10-03 19:38:48 +0530

    sort method used to sort companies by start year

--- main.js
+++ main.js
@@ -13,6 +13,25 @@ const companies = [
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 
+
+// sort()
+// sort companies by start year
+const sortedCompanies = companies.sort(function(c1, c2){
+
+    // company 1 start year > company 2 start year
+    if(c1.start > c2.start){
+
+        return 1;
+
+    } else {
+
+        return -1;
+
+    }
+});
+console.log(sortedCompanies);
+
+
 // map()
 // create arrays using map()
 
